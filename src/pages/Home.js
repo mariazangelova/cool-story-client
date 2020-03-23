3b10385cc43ad3916d3eb0a552dbb16b5d31b762
mariazangelova <mariazangelova@gmail.com>
2020-03-23 15:39:04 +0100

    Fetch homepages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,38 @@
 import React, { useEffect } from "react";
 import { Jumbotron } from "react-bootstrap";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getHomepagesThunk } from "../store/home/actions";
+import { selectHomepages } from "../store/home/selectors";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 
 export default function Home() {
   const dispatch = useDispatch();
+  const homepages = useSelector(selectHomepages);
+  console.log("homepages", homepages);
   useEffect(() => {
     dispatch(getHomepagesThunk());
   }, [dispatch]);
   return (
-    <Jumbotron>
-      <h1>Home</h1>
-    </Jumbotron>
+    <div>
+      <Jumbotron>
+        <h1>Home</h1>
+      </Jumbotron>
+      {homepages.map(homepage => {
+        return (
+          <Container key={homepage.id}>
+            <div
+              style={{
+                color: homepage.color,
+                background: homepage.backgroundColor
+              }}
+            >
+              <h3>{homepage.title}</h3>
+              <Button>Visit page</Button>
+            </div>
+          </Container>
+        );
+      })}
+    </div>
   );
 }
